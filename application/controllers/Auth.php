4a15f90943c1d36318c785dc8ac5f8e6a5e420e6
zulfikarahmad2006 <zulfikar.ahmad2006@gmail.com>
2026-06-26 23:39:46 +0800

    Registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,8 +8,10 @@ class Auth extends CI_Controller
         parent::__construct();
         $this->load->library('form_validation');
     }
+
     public function index()
     {
+        $data['title'] = 'Login Page';
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
@@ -17,18 +19,60 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        if( $this->form_validation->run() == false){
-            $data['title'] = 'WPU User Registration'
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+
+        $this->form_validation->set_rules(
+            'email',
+            'Email',
+            'required|trim|valid_email|is_unique[user.email]',
+            [
+                'is_unique' => 'This email has been registered.'
+            ]
+        );
+
+        $this->form_validation->set_rules(
+            'password1',
+            'Password',
+            'required|trim|min_length[3]|matches[password2]',
+            [
+                'matches' => "Password doesn't match!",
+                'min_length' => 'Password too short'
+            ]
+        );
+
+        $this->form_validation->set_rules(
+            'password2',
+            'Password',
+            'required|trim|matches[password1]'
+        );
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU User Registration';
+
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'Data Berhasil Ditambahkan!'
-        }
-        $data['title'] = 'WPU User Registration';
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
 
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+            $this->db->insert('user', $data);
+
+            $this->session->set_flashdata(
+                'message',
+                '<div class="alert alert-success" role="alert">
+                    Congratulation! Your account has been created. Please login
+                </div>'
+            );
+
+            redirect('auth');
+        }
     }
 }

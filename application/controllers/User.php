fad495114d4a807a13c195dda46b56793fd43c6d
24010110081-Dzaky-Imam-Akbari <24010110081@universitasbumigora.ac.id>
2026-06-27 20:48:12 +0800

    Mmeperbaiki Kesalahan Spasi pada header.php & kesalahan pada index.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -8,7 +8,7 @@ class User extends CI_Controller
         $data['title'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
-        
+
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);

98b319cefd9c64f1e9e15b999461cde3ca6294cf
Toriq <toriqksm70@gmail.com>
2026-06-28 18:00:38 +0800

    perbaikin menu sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fa-solid fa-cat"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">PETCONNECT></div>
+        <div class="sidebar-brand-text mx-3">PETCONNECT</div>
     </a>
 
     <!-- Divider -->
@@ -15,11 +15,11 @@
     <!-- QUERY MENU -->
     <?php
     $role_id = $this->session->userdata('role_id');
-    $queryMenu = "SELECT 'user_menu'.'id', 'menu'
-                    FROM 'user_menu' JOIN 'user_access_menu'
-                    ON 'user_menu'.'id' = 'user_access_menu'.'menu_id'
-                    WHERE 'user_access_menu'.'role_id' = $role_id
-                    ORDER BY 'user_access_menu'.'menu_id' ASC
+    $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                    FROM `user_menu` JOIN `user_access_menu`
+                    ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                    WHERE `user_access_menu`.`role_id` = $role_id
+                    ORDER BY `user_access_menu`.`menu_id` ASC
                     ";
     $menu = $this->db->query($queryMenu)->result_array();
     ?>
@@ -34,10 +34,10 @@
     <?php
     $menuId = $m['id'];
     $querySubMenu = "SELECT *
-                    FROM 'user_sub_menu' JOIN 'user_menu'
-                    ON 'user_sub_menu'.'menu_id' = 'user_menu'.'id'
-                    WHERE 'user_sub_menu'.'menu_id' = $menuId
-                    AND 'user_sub_menu'.'is_active' = 1
+                    FROM `user_sub_menu` JOIN `user_menu`
+                    ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                    WHERE `user_sub_menu`.`menu_id` = $menuId
+                    AND `user_sub_menu`.`is_active` = 1
                     ";
     $subMenu = $this->db->query($querySubMenu)->result_array();
     ?>

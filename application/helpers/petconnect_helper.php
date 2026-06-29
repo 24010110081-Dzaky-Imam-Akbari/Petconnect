a9d749cfbf842f0e42f0cc1eb0ed7226d7a77d11
24010110081-Dzaky-Imam-Akbari <24010110081@universitasbumigora.ac.id>
2026-06-29 19:07:20 +0800

    memperbaiki kesalahan penulisan di petconnect_helper.php

--- application/helpers/petconnect_helper.php
+++ application/helpers/petconnect_helper.php
@@ -13,7 +13,7 @@ function is_logged_in()
         $queryMenu = $CI->db->get_where('user_menu', ['menu' => $menu])->row_array();
         $menu_id= $queryMenu['id'];
 
-        $ucerAccess = $CI->db->get_where('$user_access_menu', ['role_id' => $role_id, 'menu_id' => $menu_id]);
+        $ucerAccess = $CI->db->get_where('user_access_menu', ['role_id' => $role_id, 'menu_id' => $menu_id]);
         if ($ucerAccess->num_rows() < 1) {
             redirect('auth/blocked');
         }
